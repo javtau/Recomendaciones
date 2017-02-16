ea99b2bd2745edb8c2e8e80e29889c4fabc73dc0
javtau <javier.lozano.almeda@gmail.com>
2017-02-16 11:33:35 +0100

    Initial commit

---
+++ app/src/main/java/com/javier_lozano/recomendaciones/Etzy.java
@@ -0,0 +1,9 @@
+package com.javier_lozano.recomendaciones;
+
+/**
+ * Created by javi0 on 15/02/2017.
+ */
+
+public class Etzy {
+    private static final String API_KEY = "yz7myvziiih35xl6kutthkwa";
+}
